8e360e47689ffe10b9e105c3a5730279e4c433b4
hectormaoglb <h.gonzalez@globant.com>
2019-05-20 17:22:28 -0500

    Add feature PROJ-0002 + PROJ-0001

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 print('Hello World from master')
 print('Hello World from develop')
 print('Hello World from PROJ-0002')
+print('Hello World from PROJ-0001')
+print('Hello World from PROJ-0001 - Change 2')
